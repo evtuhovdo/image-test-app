82c938577bca311265b3e2071e853932e3e0bc79
Denis Evtukhov <evtuhovdo@gmail.com>
2018-03-21 11:47:35 +0700

    Добавлена кнопка New.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Header from './../Header';
 import MainContent from '../MainContent';
+import Button from '../../components/Button';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <MainContent>
+          <Button text="NEW" />
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
